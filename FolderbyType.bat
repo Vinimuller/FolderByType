2689506929fa67fb93580dd1bce62034efd92702
Vinícius Müller Silveira <vini.s.muller@gmail.com>
2018-04-09 22:31:30 -0300

    faster code for only .nef .jpg and .mov files

--- FolderbyType.bat
+++ FolderbyType.bat
@@ -1,11 +1,14 @@
-@echo off
-rem For each file in your folder
-for %%a in (".\*") do (
-rem check if the file has an extension and if it is not our script
-if "%%~xa" NEQ ""  if "%%~dpxa" NEQ "%~dpx0" (
-rem check if extension folder exists, if not it is created
-if not exist "%%~xa" mkdir "%%~xa"
-rem Move the file to directory
-move "%%a" "%%~dpa%%~xa\"
-))
-del .\FolderbyType.bat
+
+set /p path="Enter folder path: "
+
+cd %path%
+
+IF EXIST "*.JPG" mkdir JPG
+IF EXIST "*.NEF" mkdir NEF
+IF EXIST "*.MOV" mkdir MOV
+
+move "*.JPG" ./JPG
+move "*.NEF" ./NEF
+move "*.MOV" ./MOV
+
+pause
